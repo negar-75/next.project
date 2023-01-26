8eefd157edf59eb3ed37a7d1958ab65561931db4
Negar <nasirinegar13@gmail.com>
2023-01-26 17:47:55 +0300

    created

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { config, library } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
+config.autoAddCss = false;
+library.add(faArrowRight);
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
